8029741de25a96347c9ec634fa6a50aae137ad1d
QuocTran3112 <quoctranworkspace@gmail.com>
2024-10-12 21:13:51 +0700

    Complete category and or entities

--- src/main/java/com/example/Restaurant/model/CategoryEntity.java
+++ src/main/java/com/example/Restaurant/model/CategoryEntity.java
@@ -1,10 +1,58 @@
 package com.example.Restaurant.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Table(name = "tbl_category")
 @Entity
 public class CategoryEntity extends BaseEntity {
+    @Column(name = "category_name")
+    private String categoryName;
 
+    @Column(name = "category_description")
+    private String categoryDescription;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "category")
+    private Set<ProductEntity> products = new HashSet<>();
+
+    public void addProduct(ProductEntity product) {
+        products.add(product);
+        product.setCategory(this);
+    }
+
+    public void deleteProduct(ProductEntity product) {
+        products.remove(product);
+        product.setCategory(null);
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getCategoryDescription() {
+        return categoryDescription;
+    }
+
+    public void setCategoryDescription(String categoryDescription) {
+        this.categoryDescription = categoryDescription;
+    }
+
+    public Set<ProductEntity> getProducts() {
+        return products;
+    }
+
+    public void setProducts(Set<ProductEntity> products) {
+        this.products = products;
+    }
 }
--- src/main/java/com/example/Restaurant/model/OrderEntity.java
+++ src/main/java/com/example/Restaurant/model/OrderEntity.java
@@ -1,5 +1,8 @@
 package com.example.Restaurant.model;
 
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
@@ -7,4 +10,69 @@ import jakarta.persistence.Table;
 @Entity
 public class OrderEntity extends BaseEntity {
 
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "total_price", precision = 13, scale = 2, nullable = false)
+    private BigDecimal totalPrice;
+
+    @Column(name = "customer_name")
+    private String customerName;
+
+    @Column(name = "customer_address")
+    private String customerAddress;
+
+    @Column(name = "customer_phone")
+    private String customerPhone;
+
+    @Column(name = "customer_email")
+    private String customerEmail;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public BigDecimal getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(BigDecimal totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerAddress() {
+        return customerAddress;
+    }
+
+    public void setCustomerAddress(String customerAddress) {
+        this.customerAddress = customerAddress;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public String getCustomerPhone() {
+        return customerPhone;
+    }
+
+    public void setCustomerPhone(String customerPhone) {
+        this.customerPhone = customerPhone;
+    }
 }
